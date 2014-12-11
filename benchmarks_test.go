49525abab9972f41f3cae0529917469232903b18
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-12-11 16:29:19 -0500

    Add format benchmark for with ext and without

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -16,6 +16,16 @@ func BenchmarkMatcher(b *testing.B) {
 
 func BenchmarkMatcherWithFormat(b *testing.B) {
 	p := "/posts/comments/:id.:format"
+	u := "/posts/comments/new.html"
+
+	i := 0
+	for ; i < b.N; i++ {
+		Match(p, u)
+	}
+}
+
+func BenchmarkMatcherWithFormatNoExt(b *testing.B) {
+	p := "/posts/comments/:id.:format"
 	u := "/posts/comments/new"
 
 	i := 0
@@ -25,4 +35,5 @@ func BenchmarkMatcherWithFormat(b *testing.B) {
 }
 
 // BenchmarkMatcher                 5000000               337 ns/op
-// BenchmarkMatcherWithFormat       5000000               346 ns/op
+// BenchmarkMatcherWithFormat       5000000               574 ns/op
+// BenchmarkMatcherWithFormatNoExt  5000000               345 ns/op
