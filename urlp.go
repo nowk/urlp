a4a9118ca28096a92dc73ef6013fb0eaff496ba3
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-04-19 22:04:36 -0400

    Add Path struct with breakdown of pattern

---
+++ urlp.go
@@ -0,0 +1,80 @@
+package urlp
+
+type node string
+
+func (n node) IsParam() bool {
+	if len(n) <= 1 {
+		return false
+	}
+
+	return n[1] == ':'
+}
+
+type Path struct {
+	Pattern string
+	Nodes   []node
+	Params  int
+}
+
+func NewPath(pat string) *Path {
+	nodes, n := parseNodes(pat)
+
+	return &Path{
+		Pattern: pat,
+		Nodes:   nodes,
+		Params:  n,
+	}
+}
+
+func parseNodes(pat string) ([]node, int) {
+	if pat == "/" || pat == "" {
+		return []node{"/"}, 0
+	}
+
+	n := make([]node, 0, nodeCount(pat))
+	m := len(pat)
+	p := 0
+	i := 0
+	c := 0
+	for {
+		c++
+
+		if (c == m || pat[c] == '/') && c > 0 {
+			no := node(pat[i:c])
+			if no == "/" {
+				break
+			}
+			if no[1] == ':' {
+				no = no[1:] // strip off / prefix
+				p++
+			}
+
+			n = append(n, no)
+			i = c
+		}
+
+		if c >= m {
+			break
+		}
+	}
+
+	return n, p
+}
+
+func nodeCount(pat string) int {
+	var lastchar rune
+	n := 0
+	for _, v := range pat {
+		if v == '/' {
+			n++
+		}
+
+		lastchar = v
+	}
+
+	if lastchar == '/' {
+		n--
+	}
+
+	return n
+}
